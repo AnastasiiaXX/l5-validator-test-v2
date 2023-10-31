143644aa2db1fcc52d43c8db5f77c647ad4bd64f
AnastasiiaXX <117952341+AnastasiiaXX@users.noreply.github.com>
2023-10-31 11:20:08 +0300

    Update StringSchema.js

---
+++ src/StringSchema.js
@@ -0,0 +1,31 @@
+export default class StringSchema {
+    // validators = [(value) => typeof value === 'string'];
+    constructor(validators) {
+        this.validators = [...validators];
+      }
+    // isValid(value) {
+    //   const checks = this.validators.map((validator) => validator(value));
+    //   return !checks.includes(false);
+    // }
+
+    isValid(string) {
+       return this.validators.every((validator) => validator(string));
+    }
+    
+      startsFromUpperCase() {
+        const forbiddenValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '?', ' '];
+        const validator = (value) => (value.length > 0 ? value[0].toUpperCase() === value[0]
+          && !forbiddenValues.includes(value[0]) : false);
+        return new StringSchema([...this.validators, validator]);
+      }
+    
+      length(num) {
+        const validator = (value) => value.length === num;
+        return new StringSchema([...this.validators, validator]);
+      }
+    
+      hasExclamation() {
+        const validator = (value) => value.includes('!');
+        return new StringSchema([...this.validators, validator]);
+      }
+}
