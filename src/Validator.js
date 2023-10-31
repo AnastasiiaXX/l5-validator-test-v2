275ce578afcb0390acb8e84eed6d5e4634a8a6d6
AnastasiiaXX <117952341+AnastasiiaXX@users.noreply.github.com>
2023-10-31 11:19:43 +0300

    Update Validator.js

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,18 @@
+import StringSchema from './StringSchema.js';
+import ArraySchema from './ArraySchema.js';
+import ObjectSchema from './ObjectSchema.js';
 
+export default class Validator {
+    string() {
+        return new StringSchema([(value) => typeof value === 'string']);
+      }
+   
+
+    array() {
+        return new ArraySchema;
+    } 
+
+    object() {
+        return new ObjectSchema();
+      }
+}
