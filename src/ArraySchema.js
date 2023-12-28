8c5a2672f44b12fc732c75c781ddcb3c2c884a46
Anastasiia <bjko.smsng@gmail.com>
2023-12-28 03:00:29 +0300

    add step 4

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,5 +1,5 @@
 export default class ArraySchema {
-    validators = [(value) => Array.isArray(value)];
+  validators = [(value) => Array.isArray(value)];
 
   isValid(value) {
     const checks = this.validators.map((validator) => validator(value));
@@ -8,6 +8,30 @@ export default class ArraySchema {
 
   length(number) {
     const validator = (value) => (value !== null ? value.length === number : false);
+    this.validators.push(validator);
+    return this;
+  }
+
+  maxDepth(max) {
+    const validator = (array) => {
+      // ф-я принимает текущий эл-т и глубину по умолчанию равную -1
+      // если мы попали не в массив, а в сам элемент (первый тест)
+      // 0 нельзя, т.к. далее, прибавив 1, получим уровень вложенности 1,
+      // а вложенности в случае [1, 2, 3] нет
+      const iter = (el, depth = -1) => {
+        // базовый случай - не массив, возвращает глубину (первый тест -> -1)
+        if (!Array.isArray(el)) {
+          return depth;
+        }
+        // если след. элемент - массив, проходим мапом по нему, рекурсивно запускаем ф-ю iter
+        // и увеличиваем глубину на 1
+        const result = el.map((value) => iter(value, depth + 1));
+        // возвращается (после мап) массив из чисел, обозначающих глубину
+        // Math.max возвращает самое большое число (наибольший уровень вложенности)
+        return Math.max(...result);
+      };
+      return iter(array) <= max;
+    };
     this.validators.push(validator);
     return this;
   }
--- src/Validator.js
+++ src/Validator.js
@@ -1,18 +1,18 @@
+/* eslint-disable class-methods-use-this */
 import StringSchema from './StringSchema.js';
 import ArraySchema from './ArraySchema.js';
 import ObjectSchema from './ObjectSchema.js';
 
 export default class Validator {
-    string() {
-        return new StringSchema([(value) => typeof value === 'string']);
-      }
-   
+  string() {
+    return new StringSchema([(value) => typeof value === 'string']);
+  }
 
-    array() {
-        return new ArraySchema;
-    } 
+  array() {
+    return new ArraySchema();
+  }
 
-    object() {
-        return new ObjectSchema();
-      }
+  object() {
+    return new ObjectSchema();
+  }
 }
